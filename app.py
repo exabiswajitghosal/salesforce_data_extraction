8dc656c01645b2cc64b7d7b0459661a82bceb6e1
Biswaji Ghosal <biswajit.ghosal@exavalu.com>
2025-07-02 12:04:28 +0530

    adding landing ai features

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from uuid import uuid4
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
@@ -12,6 +13,7 @@ logger = logging.getLogger(__name__)
 # Import your functions
 from utils.data_extraction_openAI import match_extracted_with_template_from_images
 from utils.pdf_to_img_base64 import convert_pdf_base64_to_image_base64s
+from utils.data_extraction_landingAI import match_extracted_with_template
 
 # Create FastAPI instance
 app = FastAPI()
@@ -38,9 +40,30 @@ def index():
     return {"message": "You're connected successfully."}
 
 
-# POST endpoint for PDF extraction
-# @app.post("/api/extract_from_pdf_base64")
-# async def extract_from_pdf_base64(request: PDFBase64Request):
+@app.post("/api/extract_from_pdf_base64")
+async def extract_pdf_base64_landing_ai(request: PDFBase64Request):
+    pdf_base64 = request.pdf_base64
+    form_type = request.form_type
+
+    if not form_type or not pdf_base64:
+        raise HTTPException(status_code=400, detail="Form type and PDF base64 data are required.")
+
+    if form_type not in ["125"]:
+        raise HTTPException(status_code=400, detail="Invalid form type. Supported types: 125, 127a, 137")
+
+    try:
+        response = match_extracted_with_template(pdf_base64=pdf_base64, submission_id=str(uuid4()),
+                                                 form_type=form_type)
+        if not response:
+            raise HTTPException(status_code=500, detail="Extraction returned no result.")
+    except Exception as e:
+        logger.error(f"Error extracting structured data: {e}")
+        raise HTTPException(status_code=500, detail="Failed to extract structured data.")
+    return response
+
+
+# @app.post("/api/extract_from_pdf_base64_openai")
+# async def extract_from_pdf_base64_openai(request: PDFBase64Request):
 #     pdf_base64 = request.pdf_base64
 #     form_type = request.form_type
 #
@@ -80,29 +103,28 @@ def index():
 #         "message": "Data extracted successfully.",
 #         "response": result
 #     }
-
-
-@app.post("/api/extract_from_pdf_base64")
-async def read_output(request: PDFBase64Request):
-    pdf_base64 = request.pdf_base64
-    form_type = request.form_type
-
-    if not pdf_base64:
-        raise HTTPException(status_code=400, detail="PDF base64 data is required.")
-
-    if not form_type:
-        raise HTTPException(status_code=400, detail="Form type is required.")
-
-    if form_type not in ["125"]:
-        raise HTTPException(status_code=400, detail="Invalid form type. Supported types: 125, 127a, 137")
-
-    with open('utils/template/Commercial_auto_application_data.json') as f:
-        output = f.read()
-        response = json.loads(output)
-    return response
+#
+#
+# @app.post("/api/extract_from_pdf_base64_read_output")
+# async def read_output(request: PDFBase64Request):
+#     pdf_base64 = request.pdf_base64
+#     form_type = request.form_type
+#
+#     if not pdf_base64 or not form_type:
+#         raise HTTPException(status_code=400, detail="PDF base64 data and form type are required.")
+#
+#     if form_type not in ["125"]:
+#         raise HTTPException(status_code=400, detail="Invalid form type. Supported types: 125, 127a, 137")
+#
+#     with open('utils/template/Commercial_auto_application_data.json') as f:
+#         output = f.read()
+#         response = json.loads(output)
+#     return response
 
 
 # Uvicorn server (when running directly)
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("app:app", host="0.0.0.0", port=8000)
--- utils/data_extraction_landingAI.py
+++ utils/data_extraction_landingAI.py
@@ -1,36 +1,96 @@
-import requests
 import os
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+import json
+import base64
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Get the API key from environment
 api_key = os.getenv("LANDINGAI_API_KEY")
+openai_api_key = os.getenv('OPENAI_API_KEY')
+openai_model = os.getenv('OPENAI_MODEL', 'gpt-4o-mini')
 if not api_key:
     raise ValueError("Missing API key. Make sure LANDINGAI_API_KEY is set in your .env file.")
 
-url = "https://api.va.landing.ai/v1/tools/agentic-document-analysis"
+# AgenticDoc expects 'VISION_AGENT_API_KEY'
+os.environ["VISION_AGENT_API_KEY"] = api_key
 
-headers = {
-    "Authorization": f"Bearer {api_key}"
-}
+from agentic_doc.parse import parse
 
-data = {
-    "include_marginalia": "true",
-    "include_metadata_in_markdown": "true"
-}
+def extract_data_landing_ai(pdf_base64: str):
+    if pdf_base64.startswith("data:"):
+        pdf_base64 = pdf_base64.split(",", 1)[1]
+    pdf_bytes = base64.b64decode(pdf_base64)
+    result = parse(pdf_bytes)
+    print(f"Final result: {result[0]}")
+    return result[0]
 
-file_path = "../docs/Acord-125.pdf (1).pdf"
-with open(file_path, "rb") as f:
-    files = {
-        "file": f
-    }
-    response = requests.post(url, headers=headers, data=data, files=files)
 
-print(response.status_code)
-try:
-    print(response.json())
-except Exception as e:
-    print("Error parsing JSON:", e)
-    print(response.text)
+def match_extracted_with_template( pdf_base64: str,submission_id: str, form_type: str):
+    """Matches extracted raw insights with the target template using LLM."""
+    data = extract_data_landing_ai(pdf_base64)
+    if not data:
+        return None
+    try:
+        model = ChatOpenAI(model=openai_model, temperature=0)
+        if form_type == "125":
+            template_path = "utils/template/125_JSON_Schema.json"
+            with open(template_path,'r') as f:
+                structure = f.read()
+        elif form_type == "127a":
+            return None
+            # with open('utils/template/127a_JSON_Schema.json') as f:
+            #     structure = f.read()
+        elif form_type == "137":
+            return None
+            # with open('utils/template/127b_JSON_Schema.json') as f:
+            #     structure = f.read()
+        else:
+            print(f"Unsupported form type: {form_type}")
+            return None
+
+
+        system_prompt = (
+            "You are an AI assistant specialized in extracting structured information from documents. "
+            "Please analyze the following extracted data and map it to the structure provided. "
+            "Replace <value> placeholders with actual values. If a value is missing, leave it blank."
+            "if there is any checkbox in the data, please return it as a boolean value."
+            "if not checked put false\n"
+            "Strictly follow the provided JSON structure and do not add any additional fields.\n\n"
+            f"Return the full JSON structure:\n{structure}"
+        )
+
+        response = model.invoke([
+            SystemMessage(content=system_prompt),
+            HumanMessage(content=f"Please extract the information from the following text:\n\n{data}")
+        ])
+
+        response_text = response.content
+        json_start = response_text.find('{')
+        json_end = response_text.rfind('}') + 1
+
+        if json_start == -1 or json_end == -1:
+            print("No JSON array found in final structured response.")
+            return None
+
+        parsed_response = json.loads(response_text[json_start:json_end])
+
+        # Save mapped template output
+        output_path = f"./output/{submission_id}/output.json"
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        with open(output_path, "w") as f:
+            json.dump(parsed_response, f, indent=4)
+
+        return parsed_response
+
+    except json.JSONDecodeError as e:
+        print(f"JSON Decode Error: {e}")
+        return None
+    except Exception as e:
+        print(f"Error processing structured extraction: {e}")
+        return None
+    finally:
+        print("process done")
